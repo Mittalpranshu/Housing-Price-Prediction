990d20dfb0cc919c38958706d439a70968d77763
Mittalpranshu <156687990+Mittalpranshu@users.noreply.github.com>
2025-09-15 04:19:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split,cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+import pickle
+
+#DATA LOADING
+df=pd.read_csv("dataset.csv")
+print(df.shape)
+df.head()
